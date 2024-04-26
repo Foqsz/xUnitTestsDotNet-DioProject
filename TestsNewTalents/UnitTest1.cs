2f29bfd54aa9e98a363eb9ba21dac98395c5df2e
Victor Vinicius <victormafiavinicius@gmail.com>
2024-04-26 10:29:50 -0300

    Add project files.

---
+++ TestsNewTalents/UnitTest1.cs
@@ -0,0 +1,13 @@
+using NewTalentsProcject;
+
+namespace TestsNewTalents
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Calculadora calc = new Calculadora();
+        }
+    }
+}
