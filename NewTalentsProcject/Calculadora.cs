4883649b04b6e8c538ccd17f3314e37d42b31cb1
Victor Vinicius <victormafiavinicius@gmail.com>
2024-04-26 11:13:32 -0300

    adicionado historico

--- NewTalentsProcject/Calculadora.cs
+++ NewTalentsProcject/Calculadora.cs
@@ -8,33 +8,49 @@ namespace NewTalentsProcject
 {
     public class Calculadora
     {
+        private List<string> listaHistorico;
+
+        public Calculadora()
+        {
+            listaHistorico = new List<string>();
+        }
+
         public int somar(int val1, int val2)
         {
             int res = val1 + val2;
+
+            listaHistorico.Insert(0, "Res: " + res);
             return res;
         }
 
         public int subtrair(int val1, int val2)
         {
             int res = val1 - val2;
+
+            listaHistorico.Insert(0, "Res: " + res);
             return res;
         }
 
         public int multiplicar(int val1, int val2)
         {
             int res = val1 * val2;
+
+            listaHistorico.Insert(0, "Res: " + res);
             return res;
         }
 
         public int dividir(int val1, int val2)
         {
             int res = val1 / val2;
+
+            listaHistorico.Insert(0, "Res: " + res);
             return res;
         }
 
         public List<string> historico()
         {
-            return new List<string>();
+            listaHistorico.RemoveRange(3, listaHistorico.Count - 3);
+            return listaHistorico;
         }
     }
 }
